d59470a8e0547026fa97010cbbcc5784332cc2a4
ImtiazKabir <imtiazkabir.imtiaz@gmail.com>
2025-06-24 21:13:29 +0600

    initial commit

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#define USING_NAMESPACE_IMK_LOG
+
+#define SLUG_IMK_DIR_ROOT imklib
+#include "imklib/IMK_index_ref.slug"
+
+#include SLUG_IMK_HEADER_LOG
+
+int main(void) {
+  Log(LOG_INFO, "Hello, world!");
+  return 0;
+}
